7f6e1d3252a9a3b245d17cdc2fc088aa2112e602
Giovanni Arsenius <garsenius@odecee.com.au>
2016-06-15 14:09:25 +1000

    Refactored test methods and added new test

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -1,5 +1,7 @@
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import org.junit.Test;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -14,10 +16,10 @@ import static org.junit.Assert.*;
 public class APITest {
 
     String baseURL = "http://jsonplaceholder.typicode.com";
-    String path = "/posts";
+    String path = "/posts/1";
 
     @Test
-    public void testStatus(){
+    public void verifyStatus(){
 
         String url = baseURL + path;
 
@@ -25,14 +27,14 @@ public class APITest {
                 .headers("Cache-Control", "no-cache", "Connection", "keep-alive")
                 .contentType(ContentType.JSON)
                 .get(url).getStatusCode();
-        System.out.println(url);
+        System.out.println("testStatus url: " + url);
 
        assertTrue(statuscode == 200);
 
     }
 
     @Test
-    public void testResponseType(){
+    public void verifyResponseType(){
 
         String url = baseURL + path;
 
@@ -41,8 +43,34 @@ public class APITest {
                 .then()
                 .assertThat()
                 .header("Content-Type", "application/json; charset=utf-8");
+        System.out.println("testResponseType url: " + url);
 
     }
+
+    @Test
+    public void verifyTitleInResponseBody(){
+
+        String url = baseURL + path;
+
+        Response response = given()
+                .get(url)
+                .then()
+                .extract()
+                .response();
+        System.out.println("testBody url: " + url + ' ' + response);
+
+        /* Get Response Body
+        ResponseBody body = response.getBody();
+        System.out.println(body);
+        */
+
+        String bodyTitle = response.path("title");
+        assertTrue(bodyTitle.equals("sunt aut facere repellat provident occaecati excepturi optio reprehenderit"));
+        System.out.println("title in body is: " +bodyTitle);
+
+
+    }
+
 
 
 
